ed05cd112eb584017fb98bc2ac2346380b37c4fa
Khalil Amin <kamin86@gmail.com>
2024-01-25 21:45:09 -0500

    Final update with breaks

--- student_database.py
+++ student_database.py
@@ -1,4 +1,12 @@
 """
+create lists
+print out welcome
+ask for input
+print out output
+ask for additional info
+validate input is correct
+output info
+ask for input to repeat
 
 """
 
@@ -14,18 +22,30 @@ while validation == "y":
     student = int(input(f"Enter a number 1-{len(student_name)}: "))
     print(f'Student {student} is {student_name[student - 1]}. What would you like to know?')
     category = input('Enter "hometown" or "favorite food"> ').lower()
+
     while True:
         if category == "hometown":
             print(f"{student_name[student - 1]} is from {hometown[student -1]}. ")
+            break
         elif category == "favorite food":
             print(f"{student_name[student - 1]}'s favorite food is {favorite_food[student -1]}. ")
+            break
         else:
-            category = input(f'That category does not exist. Please try again. Enter "hometown" or "favorite food"')
-        break
-    validation = input('Would you like to learn about another student? Enter "y" or "n": ').lower()
-# def valid():
-#     x = True
-#     while x == True:
-#         if category == "hometown" or category == "favorite food":
-#Please try again. Enter "hometown" or "favorite food"
+            category = input(f'That category does not exist. Please try again. Enter "hometown" or "favorite food" ')
 
+    validation = input('Would you like to learn about another student? Enter "y" or "n": ').lower()
+
+    # choose_category = True
+    # while choose_category:
+    #     if category == 'hometown' or category == 'favorite food':
+    #         match category:
+    #             case 'hometown':
+    #                 print(f"{student_name[student - 1]} is from {hometown[student - 1]}. ")
+    #                 choose_category = False
+    #             case 'favorite food':
+    #                 print(f"{student_name[student - 1]}'s favorite food is {favorite_food[student - 1]}. ")
+    #                 choose_category = False
+    #             case default:
+    #                 break
+    #     else:
+    #         category = input(f'That category does not exist. Please try again. Enter "hometown" or "favorite food"')
